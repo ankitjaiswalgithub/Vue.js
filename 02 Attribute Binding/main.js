4b1e25cf36986ba2b21a1fa1e629aefbedbb9683
Ankit Jais <jaiswalankit663@gmail.com>
2019-03-21 22:23:58 +0530

    Add files via upload

---
+++ 02 Attribute Binding/main.js
@@ -0,0 +1,13 @@
+var product = "Socks";
+var app = new Vue({
+	el: '#app',
+
+	data: {
+		product: "Socks and Shoes",
+		imageGreen: "green.jpg",
+		imageBlue: "blue.jpg",
+		nameGreen: "Green Socks",
+		nameBlue: "Blue Socks",
+		altText:"A pair of socks"
+	}
+})
---
+++ 03 Conditional Rendering/main.js
@@ -0,0 +1,12 @@
+// var define variable in vue
+var product = "Socks";
+var app = new Vue({
+	el: '#app',
+
+	data: {
+		product: "Socks",
+		image: "blue.jpg",
+//		inStock: false,
+		inventory: 10
+	}
+})
